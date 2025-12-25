c060eb945282c0d793094262ad879c2016019bd7
hozzijeong <ghwls0905@naver.com>
2025-12-25 13:24:28 +0900

    refactor: 고객 리스트 관련 컴포넌트 파일 변경

--- apps/frontend/src/domains/customer/customer-list/components/CustomerList.tsx
+++ apps/frontend/src/domains/customer/customer-list/components/CustomerList.tsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom'
 import { usePagination } from '@/hooks/usePagination'
 import { Pagination } from '@/components/Pagination'
 import type { Customer, Order } from '../../types'
-import { OrderDirectionItem } from './OrderDirectionItem'
 import { CustomerListSkeleton } from './CustomerListSkeleton'
 import { CustomerListErrorFallback } from './CustomerListErrorFallback'
+import { IconUpArrow } from '../../assets/IconUpArrow'
 
 const ITEMS_PER_PAGE = 10
 
@@ -74,3 +74,16 @@ export function CustomerList({ data, order, handleChangeOrder }: CustomerListPro
 
 CustomerList.Fallback = CustomerListSkeleton
 CustomerList.ErrorFallback = CustomerListErrorFallback
+
+function OrderDirectionItem({ order }: { order: Order }) {
+  return (
+    <div className="flex flex-col gap-1">
+      <IconUpArrow className={order === 'asc' ? 'text-red-800' : 'text-brand-gray-700'} width={6} height={5} />
+      <IconUpArrow
+        className={`${order === 'desc' ? 'text-red-800' : 'text-brand-gray-700'} rotate-180`}
+        width={6}
+        height={5}
+      />
+    </div>
+  )
+}
