a039e198713c16d41a73028fad1ec50901619f7a
hozzijeong <ghwls0905@naver.com>
2025-12-25 13:55:16 +0900

    feat: main 레이아웃 최소 길이 적용

--- apps/frontend/src/layout/MainLayout.tsx
+++ apps/frontend/src/layout/MainLayout.tsx
@@ -5,7 +5,7 @@ export function MainLayout({ children }: PropsWithChildren) {
   return (
     <>
       <Gnb />
-      <main className="mx-auto max-w-7xl pt-20 flex flex-col gap-15 px-4 py-8">{children}</main>
+      <main className="mx-auto max-w-7xl min-w-3xl pt-20 flex flex-col gap-15 px-4 py-8">{children}</main>
     </>
   )
 }
