7ad0c113c5820d16efc79c9071df6a429a5be50d
hozzijeong <ghwls0905@naver.com>
2025-12-25 13:49:51 +0900

    feat: gnb에 대시보드 링크 추가

--- apps/frontend/src/components/Gnb.tsx
+++ apps/frontend/src/components/Gnb.tsx
@@ -5,9 +5,16 @@ export function Gnb() {
   return (
     <header className="fixed w-full z-50 bg-white h-20 border-b-2 border-gray-100">
       <div className="max-w-[1440px] w-full mx-auto flex items-center h-full">
-        <Link to={'/dashboard'}>
-          <img src={DataRiseLogo} />
+        <Link to={'/dashboard'} className="h-[37px]">
+          <img src={DataRiseLogo} height={37} />
         </Link>
+        <nav className="ml-7">
+          <ul className="flex  items-center gap-4">
+            <li className="px-3">
+              <Link to={'/dashboard'}>대시보드</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   )
